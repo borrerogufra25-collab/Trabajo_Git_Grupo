fba78e800d71682970e6f315d812f2a4cd9ab56c
Adrián Sánchez <sanchez.meadr25@triana.salesianos.edu>
2026-01-08 10:48:51 +0100

    Cogido de las clases Empresa y TarjetaDeVisita

--- TrabajoGit/src/proyecto/Empresa.java
+++ TrabajoGit/src/proyecto/Empresa.java
@@ -1,5 +1,32 @@
 package proyecto;
 
 public class Empresa {
-
+	
+	private String nombres;
+	private String CIF;
+	
+	//------------------------------------------------
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getCIF() {
+		return CIF;
+	}
+	public void setCIF(String cIF) {
+		CIF = cIF;
+	}
+	
+	//--------------------------------------------------
+	
+	public Empresa(String nombres, String cIF) {
+		super();
+		this.nombres = nombres;
+		CIF = cIF;
+	}
+	
+	
 }
+
--- TrabajoGit/src/proyecto/TarjetaDeVisita.java
+++ TrabajoGit/src/proyecto/TarjetaDeVisita.java
@@ -1,5 +1,38 @@
 package proyecto;
 
-public class TarjetaDeVisita {
+public class TarjetaDeVisita extends Documento {
+	private String nombreT;
+	private String telefono;
+	
+	//-----------------------------------------------------------------------
+	public String getNombreT() {
+		return nombreT;
+	}
+	public void setNombreT(String nombreT) {
+		this.nombreT = nombreT;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		telefono = telefono;
+	}
 
+	
+	//-------------------------------------------------------------------------
+	
+	public TarjetaDeVisita(String nombreT, String telefono) {
+		super();
+		this.nombreT = nombreT;
+		telefono = telefono;
+	}
+
+	
+	//----------------------------------------------------------------------------
+	public void Imprimir(Empresa p) {
+		super.Imprimir(p);
+		System.out.println("*** El nombre del trabajador es: "+nombreT);
+		System.out.println("****. El teléfono es: "+telefono);
+	}
+	
 }
